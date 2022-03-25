ff82b27e232396c1c9fa94765d6719b38b1124bf
vale789092 <valeska2110@estudiantec.cr>
2022-03-24 23:09:54 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
-public class App {
+public class Main {
     public static void main(String[] args){
-        Matriz prueba = new Matriz();
-        prueba.setVisible(true);
+        //Matriz prueba = new Matriz();
+        Obstaculos nose = new Obstaculos();
+        Base prueba = new Base();
+        //prueba.setVisible(true);
 
     }
 }
