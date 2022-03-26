c767a351beb746fd19adaca3f3ea6cab8bb7ada1
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 11:31:30 -0600

    Update Amenaza.java

--- Amenaza.java
+++ Amenaza.java
@@ -4,6 +4,7 @@ public class Amenaza {
     private int fila;
     private int columna;
     private int cantidad = 0;
+    private Color color = new java.awt.Color(255, 172, 28);
 
     public Amenaza(){
         pintarAmen();
@@ -13,10 +14,10 @@ public class Amenaza {
         while(cantidad < 5){
             fila = (int) (Math.random()*(48-1)) + 1;
             columna = (int) (Math.random()*(48-1)) + 1;
-            Matriz.bMatriz[fila][columna].setBackground(Color.ORANGE);
-            Matriz.bMatriz[fila][columna+1].setBackground(Color.ORANGE);
-            Matriz.bMatriz[fila+1][columna].setBackground(Color.ORANGE);
-            Matriz.bMatriz[fila+1][columna+1].setBackground(Color.ORANGE);
+            Matriz.bMatriz[fila][columna].setBackground(color);
+            Matriz.bMatriz[fila][columna+1].setBackground(color);
+            Matriz.bMatriz[fila+1][columna].setBackground(color);
+            Matriz.bMatriz[fila+1][columna+1].setBackground(color);
             cantidad++;
         }
     }
