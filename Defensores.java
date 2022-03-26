9c656fea516fd9f7a6330d73977704a23506bf51
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 08:52:52 -0600

    Update Defensores.java

--- Defensores.java
+++ Defensores.java
@@ -1,8 +1,14 @@
 public class Defensores extends AgenteBase{
     private Matriz agente;
+    AgenteBase[] hormigaD;
 
-    public Defensores(Matriz matriz){
+    public Defensores(Matriz matriz, AgenteBase[] defensoras){
+        hormigaD = defensoras;
         agente = matriz;
+    }
+
+    public void Atacar(){
+
     }
 }
 
