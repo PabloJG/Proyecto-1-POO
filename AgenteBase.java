87e0c78bf2c1f588c69c033418fb8d7c06eb7ea7
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 08:52:35 -0600

    Update AgenteBase.java

--- AgenteBase.java
+++ AgenteBase.java
@@ -1,10 +1,10 @@
 import java.util.Random;
+import java.awt.Color;
 
 public class AgenteBase {
     private int fila = Base.random.get(Base.fila);
     private int columna = Base.random.get(Base.columna);;
     private String posicion;
-    
     private Random rand = new Random();
     private String[] posiciones = {"ARRIBA","ABAJO","IZQUIERDA","DERECHA"};
     private int pos = rand.nextInt(posiciones.length);
@@ -14,8 +14,11 @@ public class AgenteBase {
         System.out.println(fila + " " + columna + " " + posicion);
     }
 
-    public void mover(Matriz mover){
-
+    public void mover(){
+        Matriz.bMatriz[fila][columna].setBackground(Color.WHITE);
+        Base.pintarBase();
+        Matriz.bMatriz[fila][columna+1].setBackground(Color.BLACK);
+        columna++;
     }
 
     public void buscarRecurso(){
