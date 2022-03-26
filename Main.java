b592ee5faaba99114096d03f22700c5588059077
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 08:53:00 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args){
-        Matriz matriz = new Matriz();
-        Obstaculos obstaculos = new Obstaculos(matriz);
-        Base base = new Base(matriz);
-        Amenaza amenaza = new Amenaza(matriz);
-        Recursos recursos = new Recursos(matriz);
         AgenteBase[] agentes = new AgenteBase[20];
+        Matriz matriz = new Matriz(agentes);
+        Obstaculos obstaculos = new Obstaculos();
+        Base base = new Base();
+        Amenaza amenaza = new Amenaza();
+        Recursos recursos = new Recursos();
 
         for(int i = 0; i<10; i++){
-            agentes[i] = new Defensores(matriz);
+            agentes[i] = new Defensores(matriz, agentes);
         }
         for(int j = 10; j<20; j++){
-            agentes[j] = new Recolectores(matriz);
+            agentes[j] = new Recolectores(matriz, agentes);
         }
     }
 }
