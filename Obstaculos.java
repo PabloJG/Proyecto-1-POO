790972f9831c2fa8f769bbd3e42ef8174a237640
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 11:32:44 -0600

    Update Obstaculos.java

--- Obstaculos.java
+++ Obstaculos.java
@@ -4,6 +4,7 @@ public class Obstaculos {
     private int fila;
     private int columna;
     private int cantidad = 0;
+    private Color color = new java.awt.Color(158, 158, 158);
 
     public Obstaculos(){
         pintarObs();
@@ -13,10 +14,10 @@ public class Obstaculos {
         while(cantidad < 5){
             fila = (int) (Math.random()*(47-2)) + 2;
             columna = (int) (Math.random()*(47-2)) + 2;
-            Matriz.bMatriz[fila][columna].setBackground(Color.MAGENTA);
-            Matriz.bMatriz[fila][columna+1].setBackground(Color.MAGENTA);
-            Matriz.bMatriz[fila+1][columna].setBackground(Color.MAGENTA);
-            Matriz.bMatriz[fila+1][columna+1].setBackground(Color.MAGENTA);
+            Matriz.bMatriz[fila][columna].setBackground(color);
+            Matriz.bMatriz[fila][columna+1].setBackground(new java.awt.Color(216, 216, 216));
+            Matriz.bMatriz[fila+1][columna].setBackground(new java.awt.Color(132, 132, 132));
+            Matriz.bMatriz[fila+1][columna+1].setBackground(color);
             cantidad++;
         }
     }
