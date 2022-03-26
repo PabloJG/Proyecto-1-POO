1cfa4e312a3834b0179fcf24b3cb2c4a1b0a7a02
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 11:32:16 -0600

    Update Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -6,7 +6,8 @@ import javax.swing.JPanel;
 import java.awt.GridLayout;
 import java.awt.event.*;
 
-public class Matriz extends JFrame implements ActionListener{
+public class Matriz implements ActionListener{
+    static int i=0;
     private static JFrame ventana = new JFrame();
     private static JPanel boton = new JPanel();
     AgenteBase[] move;
@@ -18,6 +19,7 @@ public class Matriz extends JFrame implements ActionListener{
         move = mover;
         siguiente.addActionListener(this);
         panel.setLayout(new GridLayout(50, 50));
+        panel.setBackground(new java.awt.Color(0, 151, 7));
         ventana.add(panel);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         crearMatriz();
@@ -28,8 +30,8 @@ public class Matriz extends JFrame implements ActionListener{
             for (int c = 0; c < 50; c++) {
                 bMatriz[f][c] = new JButton();
                 bMatriz[f][c].setBounds(20, 10, 360, 360);
-                bMatriz[f][c].setBackground(Color.WHITE);
-                //bMatriz[f][c].setBorderPainted(false);
+                bMatriz[f][c].setBackground(new java.awt.Color(0, 135, 6));
+                bMatriz[f][c].setBorderPainted(false);
                 ventana.setBounds(300, 20, 850, 710);
                 panel.setBounds(300, 20, 850, 710);
                 panel.add(bMatriz[f][c]);
@@ -45,7 +47,11 @@ public class Matriz extends JFrame implements ActionListener{
     }
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == siguiente) {
-            move[0].mover();
+            i = 0;
+            while(i<20){
+                move[i].mover(move);
+                i++;
+            }
         }
       }
 }
