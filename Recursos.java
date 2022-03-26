a309d5003d8efa5660b7bd7d50b3c56c81308f9d
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 11:34:49 -0600

    Update Recursos.java

--- Recursos.java
+++ Recursos.java
@@ -13,10 +13,10 @@ public class Recursos{
         while(cantidad < 5){
             fila = (int) (Math.random()*(48-1)) + 1;
             columna = (int) (Math.random()*(48-1)) + 1;
-            Matriz.bMatriz[fila][columna].setBackground(Color.PINK);
-            Matriz.bMatriz[fila][columna+1].setBackground(Color.PINK);
-            Matriz.bMatriz[fila+1][columna].setBackground(Color.PINK);
-            Matriz.bMatriz[fila+1][columna+1].setBackground(Color.PINK);
+            Matriz.bMatriz[fila][columna].setBackground(new java.awt.Color(101, 21, 146));
+            Matriz.bMatriz[fila][columna+1].setBackground(new java.awt.Color(63, 83, 229));
+            Matriz.bMatriz[fila+1][columna].setBackground(new java.awt.Color(130, 255, 71));
+            Matriz.bMatriz[fila+1][columna+1].setBackground(new java.awt.Color(255, 90, 90));
             cantidad++;
         }
     }
