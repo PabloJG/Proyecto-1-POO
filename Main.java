6e533753c11d74fdabd13ae6b29548d1bf43d0f0
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 07:51:47 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,13 @@ public class Main {
         Base base = new Base(matriz);
         Amenaza amenaza = new Amenaza(matriz);
         Recursos recursos = new Recursos(matriz);
+        AgenteBase[] agentes = new AgenteBase[20];
+
+        for(int i = 0; i<10; i++){
+            agentes[i] = new Defensores(matriz);
+        }
+        for(int j = 10; j<20; j++){
+            agentes[j] = new Recolectores(matriz);
+        }
     }
 }
