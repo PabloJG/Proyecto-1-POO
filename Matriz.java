9068f57014e7dbbf631f7baef97eb0641d7b5ae8
vale789092 <valeska2110@estudiantec.cr>
2022-03-26 07:51:58 -0600

    Update Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -6,11 +6,11 @@ import javax.swing.JPanel;
 import java.awt.GridLayout;
 
 public class Matriz{
-    JFrame ventana = new JFrame();
-    JPanel boton = new JPanel();
-    JButton siguiente = new JButton("Siguiente");
-    JButton bMatriz[][] = new JButton[50][50];
-    JPanel panel = new JPanel();
+    private static JFrame ventana = new JFrame();
+    private static JPanel boton = new JPanel();
+    private static JButton siguiente = new JButton("Siguiente");
+    public static JButton bMatriz[][] = new JButton[50][50];
+    private static JPanel panel = new JPanel();
 
     public Matriz(){
         panel.setLayout(new GridLayout(50, 50));
@@ -19,13 +19,13 @@ public class Matriz{
         crearMatriz();
     }
 
-    public void crearMatriz(){
+    private void crearMatriz(){
         for (int f = 0; f < 50; f++) {
             for (int c = 0; c < 50; c++) {
                 bMatriz[f][c] = new JButton();
                 bMatriz[f][c].setBounds(20, 10, 360, 360);
                 bMatriz[f][c].setBackground(Color.WHITE);
-                bMatriz[f][c].setBorderPainted(false);
+                //bMatriz[f][c].setBorderPainted(false);
                 ventana.setBounds(300, 20, 850, 710);
                 panel.setBounds(300, 20, 850, 710);
                 panel.add(bMatriz[f][c]);
@@ -38,5 +38,8 @@ public class Matriz{
         siguiente.setBackground(Color.WHITE);
         ventana.add(boton, BorderLayout.EAST);
         
+    }
+    private void siguiente(AgenteBase mov){
+
     }
 }
