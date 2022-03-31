86fd922d973948bdec56e5046afc749c39621163
vale789092 <valeska2110@estudiantec.cr>
2022-03-30 23:03:43 -0600

    Update AgenteBase.java

--- AgenteBase.java
+++ AgenteBase.java
@@ -2,10 +2,9 @@ import java.util.Random;
 import java.awt.Color;
 
 public class AgenteBase {
-    static int i = 0;
     private int fila;
-    String tipo;
-    private int columna = Base.random.get(Base.columna);
+    public Color color;
+    private int columna;
     private String posicion;
     private Random rand = new Random();
     private String[] posiciones = {"ARRIBA","ABAJO","IZQUIERDA","DERECHA"};
@@ -19,43 +18,32 @@ public class AgenteBase {
 
     public void pintar(){
         System.out.println(fila + " " + columna);
-        if(i < 10)
-            Matriz.bMatriz[fila][columna].setBackground(Color.RED);
-        else
-             Matriz.bMatriz[fila][columna].setBackground(Color.BLACK);
-        i++;
+        if(Matriz.bMatriz[fila][columna].getBackground().equals(Matriz.verde))
+            Matriz.bMatriz[fila][columna].setBackground(color);
+        else{
+            fila = (int) (Math.random()*(48-1)) + 1;
+            columna = (int) (Math.random()*(48-1)) + 1;
+        }
     }
 
     public void mover(AgenteBase[] hormiga){
         Matriz.bMatriz[hormiga[Matriz.i].fila][hormiga[Matriz.i].columna].setBackground(new java.awt.Color(0, 135, 6));
-        Base.pintarBase();
+       // Base.pintarBase();
         System.out.println(hormiga[Matriz.i] + hormiga[Matriz.i].posicion);
         if(hormiga[Matriz.i].posicion.equals("ARRIBA")){
-            if(hormiga[Matriz.i].tipo.equals("defensor"))
-                Matriz.bMatriz[hormiga[Matriz.i].fila+1][hormiga[Matriz.i].columna].setBackground(Color.RED);
-            else
-                Matriz.bMatriz[hormiga[Matriz.i].fila+1][hormiga[Matriz.i].columna].setBackground(Color.BLACK);
+            Matriz.bMatriz[hormiga[Matriz.i].fila+1][hormiga[Matriz.i].columna].setBackground(color);
             fila++;
         }
         else if(hormiga[Matriz.i].posicion.equals("ABAJO")){
-            if(hormiga[Matriz.i].tipo.equals("defensor"))
-                Matriz.bMatriz[hormiga[Matriz.i].fila-1][hormiga[Matriz.i].columna].setBackground(Color.RED);
-            else
-                Matriz.bMatriz[hormiga[Matriz.i].fila-1][hormiga[Matriz.i].columna].setBackground(Color.BLACK);
+            Matriz.bMatriz[hormiga[Matriz.i].fila-1][hormiga[Matriz.i].columna].setBackground(color);
             fila--;
         }
         else if(hormiga[Matriz.i].posicion.equals("IZQUIERDA")){
-            if(hormiga[Matriz.i].tipo.equals("defensor"))
-                Matriz.bMatriz[hormiga[Matriz.i].fila][hormiga[Matriz.i].columna-1].setBackground(Color.RED);
-            else
-                Matriz.bMatriz[hormiga[Matriz.i].fila][hormiga[Matriz.i].columna-1].setBackground(Color.BLACK);
+            Matriz.bMatriz[hormiga[Matriz.i].fila][hormiga[Matriz.i].columna-1].setBackground(color);
             columna--;
         }
         else if(hormiga[Matriz.i].posicion.equals("DERECHA")){
-            if(hormiga[Matriz.i].tipo.equals("defensor"))
-                Matriz.bMatriz[hormiga[Matriz.i].fila][hormiga[Matriz.i].columna+1].setBackground(Color.RED);
-            else
-                Matriz.bMatriz[hormiga[Matriz.i].fila][hormiga[Matriz.i].columna+1].setBackground(Color.BLACK);
+            Matriz.bMatriz[hormiga[Matriz.i].fila][hormiga[Matriz.i].columna+1].setBackground(color);
             columna++;
         }  
         pos = rand.nextInt(posiciones.length);
