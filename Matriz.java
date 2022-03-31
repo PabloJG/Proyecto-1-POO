8928211bcb7bcf2112edc2f3fb65e19c36b67afe
vale789092 <valeska2110@estudiantec.cr>
2022-03-30 23:04:15 -0600

    Update Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -1,16 +1,17 @@
-import java.awt.Color;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 import java.awt.event.*;
+import java.awt.Color;
 
 public class Matriz implements ActionListener{
-    static int i=0;
+    public static int i=0;
+    public static Color verde = new java.awt.Color(0, 135, 6);
     private static JFrame ventana = new JFrame();
     private static JPanel boton = new JPanel();
-    AgenteBase[] move;
+    private AgenteBase[] move;
     private static JButton siguiente = new JButton("Siguiente");
     public static JButton bMatriz[][] = new JButton[50][50];
     private static JPanel panel = new JPanel();
@@ -30,7 +31,7 @@ public class Matriz implements ActionListener{
             for (int c = 0; c < 50; c++) {
                 bMatriz[f][c] = new JButton();
                 bMatriz[f][c].setBounds(20, 10, 360, 360);
-                bMatriz[f][c].setBackground(new java.awt.Color(0, 135, 6));
+                bMatriz[f][c].setBackground(verde);
                 bMatriz[f][c].setBorderPainted(false);
                 ventana.setBounds(300, 20, 850, 710);
                 panel.setBounds(300, 20, 850, 710);
@@ -41,7 +42,7 @@ public class Matriz implements ActionListener{
         ventana.setVisible(true);
         boton.setLayout(new GridLayout(1, 1));
         boton.add(siguiente);
-        siguiente.setBackground(Color.WHITE);
+        siguiente.setBackground(new java.awt.Color(201, 255, 237));
         ventana.add(boton, BorderLayout.EAST);
         
     }
