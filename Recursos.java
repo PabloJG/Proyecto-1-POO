7e779c8994149a4b6edc4297cac603421dcf14e5
vale789092 <valeska2110@estudiantec.cr>
2022-03-30 23:04:32 -0600

    Update Recursos.java

--- Recursos.java
+++ Recursos.java
@@ -1,9 +1,7 @@
-import java.awt.Color;
-
 public class Recursos{
     private int fila;
     private int columna;
-    private int cantidad = 0;
+    public static int cantidad = 0;
 
     public Recursos(){
         pintarRec();
@@ -13,11 +11,17 @@ public class Recursos{
         while(cantidad < 5){
             fila = (int) (Math.random()*(48-1)) + 1;
             columna = (int) (Math.random()*(48-1)) + 1;
-            Matriz.bMatriz[fila][columna].setBackground(new java.awt.Color(101, 21, 146));
-            Matriz.bMatriz[fila][columna+1].setBackground(new java.awt.Color(63, 83, 229));
-            Matriz.bMatriz[fila+1][columna].setBackground(new java.awt.Color(130, 255, 71));
-            Matriz.bMatriz[fila+1][columna+1].setBackground(new java.awt.Color(255, 90, 90));
-            cantidad++;
+            if(Matriz.bMatriz[fila][columna].getBackground().equals(Matriz.verde) && Matriz.bMatriz[fila][columna+1].getBackground().equals(Matriz.verde) && Matriz.bMatriz[fila+1][columna+1].getBackground().equals(Matriz.verde) && Matriz.bMatriz[fila+1][columna].getBackground().equals(Matriz.verde)){
+                Matriz.bMatriz[fila][columna].setBackground(new java.awt.Color(101, 21, 146));
+                Matriz.bMatriz[fila][columna+1].setBackground(new java.awt.Color(63, 83, 229));
+                Matriz.bMatriz[fila+1][columna].setBackground(new java.awt.Color(130, 255, 71));
+                Matriz.bMatriz[fila+1][columna+1].setBackground(new java.awt.Color(255, 90, 90));
+                cantidad++;
+            }
+            else{
+                fila = (int) (Math.random()*(48-1)) + 1;
+                columna = (int) (Math.random()*(48-1)) + 1;
+            }
         }
     }
     
