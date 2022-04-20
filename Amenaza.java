df8590ae408657cb4b2ebedce4456397032592a8
vale789092 <valeska2110@estudiantec.cr>
2022-04-19 22:01:04 -0600

    Update Amenaza.java

--- Amenaza.java
+++ Amenaza.java
@@ -4,14 +4,16 @@ public class Amenaza {
     private int fila;
     private int columna;
     public static int cantidadA = 0;
+    public int vida;
 
 
     public Amenaza(){
         pintarAmen();
+        vida = 10;
     }
 
     private void pintarAmen(){
-        while(cantidadA < 5){
+        while(cantidadA < 15){
             fila = (int) (Math.random()*(48-1)) + 1;
             columna = (int) (Math.random()*(48-1)) + 1;
             if(Matriz.bMatriz[fila][columna].getBackground().equals(Matriz.verde) && Matriz.bMatriz[fila][columna+1].getBackground().equals(Matriz.verde) && Matriz.bMatriz[fila+1][columna+1].getBackground().equals(Matriz.verde) && Matriz.bMatriz[fila+1][columna].getBackground().equals(Matriz.verde)){
