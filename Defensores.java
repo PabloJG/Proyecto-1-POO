7704f4b85cb50ea1edf4cafdfb71c08efb83dee8
vale789092 <valeska2110@estudiantec.cr>
2022-04-22 13:51:38 -0600

    Update Defensores.java

--- Defensores.java
+++ Defensores.java
@@ -2,12 +2,11 @@ import java.awt.Color;
 
 public class Defensores extends AgenteBase{
     private AgenteBase[] hormigaD;
-    Color tipo;
 
     public Defensores(AgenteBase[] defensores){
         hormigaD = defensores;
         color = Color.red;
-        pintar();
+        pintarH();
     }
 
     public void Atacar(){
