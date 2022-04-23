f88d95cd7845fba9df6928480b7702112bdbd2aa
vale789092 <valeska2110@estudiantec.cr>
2022-04-22 23:16:15 -0600

    Update Base.java

--- Base.java
+++ Base.java
@@ -5,16 +5,18 @@ import java.util.Random;
 public class Base {
     public static List<Integer> random = Arrays.asList(0, 49);
     private Random rand = new Random();
-    public static int fila;
-    public static int columna;
+    public static int BFila;
+    public static int BColumna;
 
     public Base(){
-        fila = rand.nextInt(random.size());
-        columna = rand.nextInt(random.size());
+        BFila = rand.nextInt(random.size());
+        BColumna = rand.nextInt(random.size());
+        BFila = random.get(BFila);
+        BColumna = random.get(BColumna);
         pintarBase(); 
     }
 
     public static void pintarBase(){
-        Matriz.bMatriz[random.get(fila)][random.get(columna)].setBackground(new java.awt.Color(144, 79, 22));
+        Matriz.bMatriz[BFila][BColumna].setBackground(new java.awt.Color(144, 79, 22));
     }
 }
