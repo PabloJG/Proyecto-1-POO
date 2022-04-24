ae30d3102de42235a44086bef43db921810bc363
vale789092 <valeska2110@estudiantec.cr>
2022-04-23 23:17:01 -0600

    Update Amenaza.java

--- Amenaza.java
+++ Amenaza.java
@@ -1,14 +1,10 @@
 import java.awt.Color;
-import java.util.LinkedList;
-import java.util.List;
-
-import javax.swing.JButton;
 
 public class Amenaza {
+    static Amenaza[] amenazas;
     private int fila;
     private int columna;
     public int cantidadA;
-    static Amenaza[] amenazas;
     int coordC1;
     int coordF1;
     int coordC2;
@@ -26,17 +22,18 @@ public class Amenaza {
         coordC2 = columna+1;
         coordF2 = fila+1;
         vida = 10;
-        pintarAmen();
     }
 
     public Amenaza(){
 
     }
 
-    private void pintarAmen(){
+    public void pintarAmen(int i){
         boolean libre = false;
+        fila = (int) (Math.random()*(48-1)) + 1;
+        columna = (int) (Math.random()*(48-1)) + 1;
         while(libre != true){
-            if(Matriz.bMatriz[fila][columna].getBackground() != (Matriz.verde) && Matriz.bMatriz[fila][columna+1].getBackground() != (Matriz.verde) && Matriz.bMatriz[fila+1][columna+1].getBackground() != (Matriz.verde) && Matriz.bMatriz[fila+1][columna].getBackground() != (Matriz.verde)){
+            if(Matriz.bMatriz[fila][columna].getBackground() != (Matriz.verde) || Matriz.bMatriz[fila][columna+1].getBackground() != (Matriz.verde) || Matriz.bMatriz[fila+1][columna+1].getBackground() != (Matriz.verde) || Matriz.bMatriz[fila+1][columna].getBackground() != (Matriz.verde)){
                 fila = (int) (Math.random()*(48-1)) + 1;
                 columna = (int) (Math.random()*(48-1)) + 1;
             }
@@ -48,44 +45,42 @@ public class Amenaza {
                 Matriz.bMatriz[fila+1][columna+1].setBackground(Color.yellow);
                 cantidadA++;
                 libre = true;
+                guardar(i);
             }
         }  
     }
 
-    public void guardar(){
-        amenazas[cantidadA].coordC1 = columna;
-        amenazas[cantidadA].coordF1 = fila;
-        amenazas[cantidadA].coordC2 = columna+1;
-        amenazas[cantidadA].coordF2 = fila+1;
+    public void guardar(int i){
+        amenazas[i].coordC1 = columna;
+        amenazas[i].coordF1 = fila;
+        amenazas[i].coordC2 = columna+1;
+        amenazas[i].coordF2 = fila+1;
+        System.out.println("hormiga numero" + i + " coordenadas " + amenazas[i].coordC1 + " "+ amenazas[i].coordF1);
     }
 
-    public void ataca(Amenaza[] reap, int F, int C){
+    public void ataca(int F, int C){
         int i = 0;
         while(i != 10){
-            System.out.println("coordF" + reap[i].coordF1 + "coordC" + reap[i].coordC1 + "coordf1" + reap[i].coordF2 + "coordc2" + reap[i].coordC2);
+            System.out.println("coordF" + amenazas[i].coordF1 + "coordC" + amenazas[i].coordC1 + "coordf1" + amenazas[i].coordF2 + "coordc2" + amenazas[i].coordC2);
             System.out.println("vienen " + F + " " + C);
-            if(reap[i].coordC1 == C && reap[i].coordF1 == F)
-                reap[i].vida--;
-            else if(reap[i].coordC2 == C && reap[i].coordF2 == F)
-                reap[i].vida--;
-            else if(reap[i].coordC1 == C && reap[i].coordF2 == F)
-                reap[i].vida--;
-            else if(reap[i].coordC2 == C && reap[i].coordF1 == F)
-                reap[i].vida--;
-            System.out.println("vidas " + i + " " + reap[i].vida);
+            if(amenazas[i].coordC1 == C && amenazas[i].coordF1 == F)
+                amenazas[i].vida--;
+            else if(amenazas[i].coordC2 == C && amenazas[i].coordF2 == F)
+                amenazas[i].vida--;
+            else if(amenazas[i].coordC1 == C && amenazas[i].coordF2 == F)
+                amenazas[i].vida--;
+            else if(amenazas[i].coordC2 == C && amenazas[i].coordF1 == F)
+                amenazas[i].vida--;
+            System.out.println("vidas " + i + " " + amenazas[i].vida);
             i++;
         }
         destruir();
     }
     public void respawn(){
         System.out.println("nuevo" + cantidadA);
-        pintarAmen();
+        pintarAmen(elim);
         amenazas[elim].vida = 10;
         System.out.println(amenazas[elim].vida);
-        amenazas[elim].coordC1 = columna;
-        amenazas[elim].coordF1 = fila;
-        amenazas[elim].coordC2 = columna+1;
-        amenazas[elim].coordF2 = fila+1;
         cantidadA++;
         System.out.println("nuevas " + "coordF" + amenazas[elim].coordF1 + "coordC" + amenazas[elim].coordC1 + "coordf1" + amenazas[elim].coordF2 + "coordc2" + amenazas[elim].coordC2);
         System.out.println(cantidadA);
@@ -103,8 +98,6 @@ public class Amenaza {
                 Matriz.bMatriz[amenazas[i].coordF1][amenazas[i].coordC2].setBackground(Matriz.verde);
                 Matriz.bMatriz[amenazas[i].coordF2][amenazas[i].coordC1].setBackground(Matriz.verde);
                 elim = i;
-                fila = (int) (Math.random()*(48-1)) + 1;
-                columna = (int) (Math.random()*(48-1)) + 1;
                 respawn();
             }
             i++;
