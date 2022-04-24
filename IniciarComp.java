dd2edf69e6b1d34224cb0af5f3cf3dc4a1eb7435
vale789092 <valeska2110@estudiantec.cr>
2022-04-23 23:17:15 -0600

    Update IniciarComp.java

--- IniciarComp.java
+++ IniciarComp.java
@@ -1,44 +1,55 @@
 public class IniciarComp {
-    private AgenteBase[] agentes;
+    static AgenteBase[] agentes;
     private Matriz matriz;
-    private Obstaculos obstaculos;
+    private Obstaculos[] obstaculos;
     private Base base;
     public static Amenaza[] amenazas;
     public static Recursos[] recursos;
     public static Amenaza amenaza = new Amenaza();
     public static Recursos recurso = new Recursos();
+    public static Obstaculos obstaculo = new Obstaculos();
 
     IniciarComp(){
         amenazas = new Amenaza[10];
-        agentes = new AgenteBase[20];
+        agentes = new AgenteBase[30];
         matriz = new Matriz(agentes);
-        obstaculos = new Obstaculos();
+        obstaculos = new Obstaculos[5];
         base = new Base();
         recursos = new Recursos[10];
         IniciarAmenaza();
         IniciarRecurso();
+        IniciarObstaculo();
         IniciarAgentes();
+    }
+
+    private void IniciarObstaculo(){
+        for(int i = 0; i<5; i++){
+            obstaculos[i] = new Obstaculos(obstaculos);
+            obstaculo.pintarObs(i);
+            obstaculo.guardar(i);
+        }
     }
 
     private void IniciarAmenaza(){
         for(int i = 0; i<10; i++){
             amenazas[i] = new Amenaza(amenazas);
-            amenaza.guardar();
+            amenaza.pintarAmen(i);
+            amenaza.guardar(i);
         }
     }
 
     private void IniciarRecurso(){
         for(int i = 0; i<10; i++){
             recursos[i] = new Recursos(recursos);
-            recurso.guardar();
+            recurso.pintarRec(i);
         }
     }
 
     private void IniciarAgentes(){
-        for(int i = 0; i<10; i++){
+        for(int i = 0; i<15; i++){
             agentes[i] = new Defensores(agentes);
         }
-        for(int j = 10; j<20; j++){
+        for(int j = 15; j<30; j++){
             agentes[j] = new Recolectores(agentes);
         }
     }
