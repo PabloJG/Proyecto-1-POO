1bd38ade12719f71c887adb199307a6e790393c8
vale789092 <valeska2110@estudiantec.cr>
2022-04-23 23:17:35 -0600

    Update Recolectores.java

--- Recolectores.java
+++ Recolectores.java
@@ -1,10 +1,8 @@
 import java.awt.Color;
 
 public class Recolectores extends AgenteBase{
-    private AgenteBase[] hormigaR;
 
     public Recolectores(AgenteBase[] recolectores){
-        hormigaR = recolectores;
         color = Color.black;
         pintarH();
     }
